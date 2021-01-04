98c67a22e2d1b3846d74723196771c55df48023e
Huntingbear21 <huntingbear21@naver.com>
2021-01-04 13:18:44 +0900

    default exports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-export class App extends React.Component {
+class App extends React.Component {
   render() {
     <div>
       <h1>준비 중입니다 . . .</h1>
     </div>
   }
 }
+
+export default App;
